942a45608b333f6f7c1b13e8c39e61813d5d7839
DESKTOP-IHOS5SD\santa239 <ongmachine@gmail.com>
2024-03-12 16:20:47 +0300

    Solution with error

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,8 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-//first string
+﻿Console.WriteLine("Program started");
 var data = "abacb";
+PrintSubstrings(data);
+Console.ReadKey();
+
+
+static void PrintSubstrings(string fullstring)
+{
+    if (string.IsNullOrEmpty(fullstring))
+        Console.WriteLine("string is empty");
+
+    if (fullstring.Length < 2)
+        Console.WriteLine("string length isn't enough");
+
+    for(int i = 1; i < fullstring.Length - 1; i++)
+    {
+        var left = fullstring.Substring(0, i);
+        var right = fullstring.Substring(i, fullstring.Length - i - 1);
+        var leftCount = GetDifferentCharsCount(left);
+        var rightCount = GetDifferentCharsCount(right);
+        if (leftCount == rightCount)
+            Console.WriteLine($"{left} - {right}");
+    }
+}
+
+static int GetDifferentCharsCount(string str)
+{
+    var listDifferentChars = new List<char>();
+    foreach(var ch in str.ToCharArray())
+    {
+        if(!listDifferentChars.Contains(ch))
+            listDifferentChars.Add(ch);
+    }
+    return listDifferentChars.Count();
+}
 
 
 
